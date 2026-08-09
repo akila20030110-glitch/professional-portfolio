109723657b97c612d7b01b5f7c3870a316625eff
AKILA SANDARUWAN <akila20030110@gmail.com>
2026-08-10 00:28:24 +0530

    Add navigation interaction

---
+++ script.js
@@ -0,0 +1,15 @@
+// Smooth scrolling for navigation links
+document.querySelectorAll('nav a[href^="#"]').forEach(link => {
+    link.addEventListener('click', function (event) {
+        const targetId = this.getAttribute('href');
+        const targetSection = document.querySelector(targetId);
+
+        if (targetSection) {
+            event.preventDefault();
+
+            targetSection.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    });
+});
